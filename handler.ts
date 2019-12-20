815ef56b5441a9de512e3b9ad11c5b8f2d8b685c
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-12-20 09:55:58 +0900

    Throw error

--- handler.ts
+++ handler.ts
@@ -1,12 +1,20 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
-
-export const hello: APIGatewayProxyHandler = async (event, _context) => {
-  return {
+export const hello = (_event, _context, callback) => {
+  callback(null, {
     statusCode: 200,
     body: JSON.stringify({
       message:
-        "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!",
-      input: event
+        "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!"
     })
-  };
+  });
+};
+
+export const helloWithException = (_event, _context, callback) => {
+  throw new Error("Uncaught Exception!");
+  callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      message:
+        "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!"
+    })
+  });
 };

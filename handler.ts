f9b19492d3301a53bc3f4e0be3632603aa1ec1b7
Shimpei Takamatsu <shimpeiws@gmail.com>
2019-12-20 10:07:48 +0900

    Connect to RDB

--- handler.ts
+++ handler.ts
@@ -1,3 +1,5 @@
+import { Client } from "pg";
+
 export const hello = (_event, _context, callback) => {
   callback(null, {
     statusCode: 200,
@@ -15,6 +17,23 @@ export const helloWithException = (_event, _context, callback) => {
     body: JSON.stringify({
       message:
         "Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!"
+    })
+  });
+};
+
+export const connectToRDB = async (_event, _context, callback) => {
+  const pgClient = new Client({
+    host: process.env.DB_HOST,
+    database: process.env.DB_DATABASE,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD
+  });
+  await pgClient.connect();
+  const res = await pgClient.query("SELECT current_timestamp");
+  await callback(null, {
+    statusCode: 200,
+    body: JSON.stringify({
+      message: JSON.stringify(res)
     })
   });
 };
